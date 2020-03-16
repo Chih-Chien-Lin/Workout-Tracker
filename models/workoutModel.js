0ac45d2e539bdc6a5851fe8a3264a70d4df0b261
Justin Lin <ro0566320@gmail.com>
2020-03-16 11:55:44 -0400

    able to get data from back end

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,67 +1,64 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [{
-        type: String,
-        name: String,
-        duration: Number,
-        distance: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number
-    }]
-});
-
-const Workout = mongoose.model("Workout", WorkoutSchema);
-module.exports = Workout;
-
-
-// var mongoose = require("mongoose");
-
-// var Schema = mongoose.Schema;
-// var workoutSchema = new Schema({
+// const WorkoutSchema = new Schema({
 //     day: {
-//       type: Date,
-//       default: () => new Date()
+//         type: Date,
+//         default: Date.now
 //     },
-//     exercises: [
-//       {
-//         type: {
-//           type: String,
-//           trim: true,
-//           required: "Enter Exercise Type"
-//         },
-//         name: {
-//           type: String,
-//           trim: true,
-//           required: "Enter Exercise Name"
-//         },
-//         duration: {
-//           type: Number,
-//           required: "Enter  Exercise Duration"
-//         },
-//         weight: {
-//           type: Number
-//         },
-//         reps: {
-//           type: Number
-//         },
-//         sets: {
-//           type: Number
-//         },
-//         distance: {
-//           type: Number
-//         }
-//       }
-//     ]
-//   }
-// );
-// var Workout = mongoose.model("Workout", workoutSchema);
-// module.exports = Workout;
+//     exercises: [{
+//         type: String,
+//         name: String,
+//         duration: Number,
+//         distance: Number,
+//         weight: Number,
+//         reps: Number,
+//         sets: Number
+//     }]
+// });
 
+// const Workout = mongoose.model("Workout", WorkoutSchema);
+// module.exports = Workout;
+//----------------------------------------------------
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var workoutSchema = new Schema({
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter Exercise Type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter Exercise Name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter  Exercise Duration"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  }
+);
+var Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,75 +1,128 @@
+// //routes the api's within pages
+// // need to create one for every fetch in api.js
 // const router = require("express").Router();
-// const db = require("../models");
-
+// const Workout = require("../models/workoutModel.js")
+// // getLastWorkout()
 // router.get("/api/workouts", (req, res) => {
-//     db.Workout.find({})
-//         .then(dbWorkouts => {
-//             res.json(dbWorkouts);
+//     console.log("getLastWorkout");
+//     Workout.find({}).sort({ date: -1 }).limit(1)
+//         .then(dbWorkout => {
+//             console.log(dbWorkout);
+//             res.json(dbWorkout)
 //         })
 //         .catch(err => {
 //             res.status(400).json(err);
 //         });
 // });
-
+// // getWorkoutsInRange() get all works
+// router.get("/api/workouts/range", (req, res) => {
+//     console.log("getWorkoutsInRange");
+//     Workout.find({}).limit(7)
+//         .then(dbWorkout => {
+//             res.json(dbWorkout)
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
+// // addExercise() is a put
+// router.put("/api/workouts/:id", (req, res) => {
+//     console.log("addExercise: ", req.body);
+//     Workout.update(
+//         { _id: req.params.id },
+//         {
+//             $push: { exercises: req.body }
+//         }).then(function (Workout) {
+//             res.json(Workout)
+//         })
+// })
+// // createWorkout() is a post
 // router.post("/api/workouts", ({ body }, res) => {
-//     // console.log(body)
-//     db.Workout.create(body)
-//         .then(dbWorkouts => {
-//             res.json(dbWorkouts);
+//     console.log("createWorkout: ", req.body);
+//     Transaction.create(body)
+//         .then(dbTransaction => {
+//             res.json(dbTransaction);
 //         })
 //         .catch(err => {
-//             res.status(400).json(err);
+//             res.status(404).json(err);
 //         });
 // });
-
-// router.put("/api/workouts/:id", function (req, res) {
-//     db.Workout.updateOne(
-//         {
-//             _id: req.params.id
-//         },
-//         {
-//             type: req.body.type
-//         },
-//         {
-//             name: req.body.name
-//         }
-//     )
-// });
-
 // module.exports = router;
 
-
-
-var db = require("../models");
-
-module.exports = function (app) {
-    app.get("/api/workouts", function (req, res) {
-        db.Workout.find({}).then(function (dbWorkouts) {
+//---------------------------------------------------------
+const router = require("express").Router();
+const db = require("../models/");
+// console.log(db.Workout.find({}))
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkouts => {
+            // console.log(dbWorkouts)
             res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
         });
-    });
+});
 
-    app.put("/api/workouts/:id", function (req, res) {
-        db.Workout.updateOne(
-            {
-                _id: req.params.id
-            },
-            {
-                type: req.body.type
-            }
-        ).then(function (dbWorkouts) {
+router.post("/api/workouts", ({ body }, res) => {
+    // console.log(body)
+    db.Workout.create(body)
+        .then(dbWorkouts => {
             res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
         });
-    });
+});
 
-    app.post("/api/workouts", ({ body }, res) => {
-        // console.log(body)
-        db.Workout.create(body)
-            .then(dbWorkouts => {
-                res.json(dbWorkouts);
-            })
-            .catch(err => {
-                res.status(400).json(err);
-            });
-    });
-};
+router.put("/api/workouts/:id", function (req, res) {
+    db.Workout.updateOne(
+        {
+            _id: req.params.id
+        },
+        {
+            type: req.body.type
+        },
+        {
+            name: req.body.name
+        }
+    )
+});
+
+module.exports = router;
+
+
+//----------------------------------------------------------------
+// var db = require("../models");
+
+// module.exports = function (app) {
+//     app.get("/api/workouts", function (req, res) {
+//         db.Workout.find({}).then(function (dbWorkouts) {
+//             res.json(dbWorkouts);
+//         });
+//     });
+
+//     app.put("/api/workouts/:id", function (req, res) {
+//         db.Workout.updateOne(
+//             {
+//                 _id: req.params.id
+//             },
+//             {
+//                 type: req.body.type
+//             }
+//         ).then(function (dbWorkouts) {
+//             res.json(dbWorkouts);
+//         });
+//     });
+
+//     app.post("/api/workouts", ({ body }, res) => {
+//         // console.log(body)
+//         db.Workout.create(body)
+//             .then(dbWorkouts => {
+//                 res.json(dbWorkouts);
+//             })
+//             .catch(err => {
+//                 res.status(400).json(err);
+//             });
+//     });
+// };
